4d3a6cc98ec578b9665693d46f0200c627df0342
Timo Jarmala <timo.jarmala@myy.haaga-helia.fi>
2020-02-03 18:27:19 +0200

    First commit

---
+++ src/main/java/com/jarmala/personRegister/controller/PersonController.java
@@ -0,0 +1,18 @@
+package com.jarmala.personRegister.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PersonController {
+
+
+    @RequestMapping(value="/hello")
+    @ResponseBody
+    public String hello(){
+
+        return "hello";
+    }
+
+}
